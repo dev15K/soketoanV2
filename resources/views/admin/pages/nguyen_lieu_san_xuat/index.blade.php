398ff2d741457dec9d9c60000405ed1df9971de0
dainq <97323194+dainqd@users.noreply.github.com>
2025-05-15 23:33:00 +0700

    feat: nguyen lieu thanh pham.

--- resources/views/admin/pages/nguyen_lieu_san_xuat/index.blade.php
+++ resources/views/admin/pages/nguyen_lieu_san_xuat/index.blade.php
@@ -69,19 +69,6 @@
                     </div>
                     <form method="post" action="{{ route('admin.nguyen.lieu.san.xuat.store') }}" class="d-none">
                         @csrf
-                        <div class="form-group">
-                            <label for="ten_nguyen_lieu">Tên nguyên liệu</label>
-                            <input type="text" class="form-control" id="ten_nguyen_lieu" name="ten_nguyen_lieu"
-                                   required>
-                        </div>
-                        <div class="form-group ">
-                            <label for="phieu_san_xuat_id">Phiếu sản xuất</label>
-                            <select id="phieu_san_xuat_id" name="phieu_san_xuat_id" class="form-control">
-                                @foreach($phieu_san_xuats as $phieu_san_xuat)
-                                    <option value="{{ $phieu_san_xuat->id }}">{{ $phieu_san_xuat->ten_phieu }}</option>
-                                @endforeach
-                            </select>
-                        </div>
                         <div class="row">
                             <div class="form-group col-md-6">
                                 <label for="ngay">Ngày</label>
@@ -89,19 +76,33 @@
                             </div>
 
                             <div class="form-group col-md-6">
-                                <label for="mau_sac">Màu sắc</label>
-                                <input type="text" class="form-control" id="mau_sac" name="mau_sac" required>
+                                <label for="phieu_san_xuat_id">Lô Sản Xuất</label>
+                                <select id="phieu_san_xuat_id" name="phieu_san_xuat_id" class="form-control">
+                                    @foreach($phieu_san_xuats as $phieu_san_xuat)
+                                        <option
+                                            value="{{ $phieu_san_xuat->id }}">{{ $phieu_san_xuat->so_lo_san_xuat }}</option>
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
+                        <div class="form-group">
+                            <label for="ten_nguyen_lieu">Tên nguyên liệu</label>
+                            <input type="text" class="form-control" id="ten_nguyen_lieu" name="ten_nguyen_lieu"
+                                   required>
+                        </div>
                         <div class="row">
-                            <div class="form-group col-md-6">
+                            <div class="form-group col-md-4">
                                 <label for="khoi_luong">Khối lượng(kg)</label>
                                 <input type="number" min="0" class="form-control" id="khoi_luong" name="khoi_luong"
                                        required>
                             </div>
-                            <div class="form-group col-md-6">
+                            <div class="form-group col-md-4">
                                 <label for="don_vi_tinh">Đơn vị tính</label>
                                 <input type="text" class="form-control" id="don_vi_tinh" name="don_vi_tinh" required>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="mau_sac">Màu sắc</label>
+                                <input type="text" class="form-control" id="mau_sac" name="mau_sac" required>
                             </div>
                         </div>
                         <div class="row">
@@ -114,20 +115,20 @@
                                 <label for="trang_thai">Trạng thái</label>
                                 <select id="trang_thai" name="trang_thai" class="form-control">
                                     <option
-                                            value="{{ \App\Enums\TrangThaiNguyenLieuSanXuat::ACTIVE() }}">{{ \App\Enums\TrangThaiNguyenLieuSanXuat::ACTIVE() }}</option>
+                                        value="{{ \App\Enums\TrangThaiNguyenLieuSanXuat::ACTIVE() }}">{{ \App\Enums\TrangThaiNguyenLieuSanXuat::ACTIVE() }}</option>
                                     <option
-                                            value="{{ \App\Enums\TrangThaiNguyenLieuSanXuat::INACTIVE() }}">{{ \App\Enums\TrangThaiNguyenLieuSanXuat::INACTIVE() }}</option>
+                                        value="{{ \App\Enums\TrangThaiNguyenLieuSanXuat::INACTIVE() }}">{{ \App\Enums\TrangThaiNguyenLieuSanXuat::INACTIVE() }}</option>
                                 </select>
                             </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="chi_tiet_khac">Chi tiết khác</label>
+                            <textarea name="chi_tiet_khac" id="chi_tiet_khac" class="form-control" rows="5"></textarea>
                         </div>
                         <div class="form-group ">
                             <label for="bao_quan">Bảo quản</label>
                             <input type="text" class="form-control" id="bao_quan" name="bao_quan"
                                    required>
-                        </div>
-                        <div class="form-group">
-                            <label for="chi_tiet_khac">Chi tiết khác</label>
-                            <textarea name="chi_tiet_khac" id="chi_tiet_khac" class="form-control" rows="5"></textarea>
                         </div>
                         <button type="submit" class="btn btn-primary mt-2">Thêm mới</button>
                     </form>
@@ -145,25 +146,29 @@
                     <table class="table table-hover vw-100">
                         <colgroup>
                             <col width="5%">
+                            <col width="10%">
+                            <col width="10%">
                             <col width="x">
-                            <col width="10%">
-                            <col width="10%">
-                            <col width="10%">
-                            <col width="10%">
-                            <col width="10%">
-                            <col width="10%">
+                            <col width="8%">
+                            <col width="8%">
+                            <col width="8%">
+                            <col width="8%">
+                            <col width="8%">
+                            <col width="8%">
                             <col width="8%">
                             <col width="8%">
                         </colgroup>
                         <thead>
                         <tr>
                             <th scope="col">#</th>
+                            <th scope="col">Ngày</th>
+                            <th scope="col">Lô Sản Xuất</th>
                             <th scope="col">Tên nguyên liệu</th>
-                            <th scope="col">Phiếu sản xuất</th>
                             <th scope="col">Khối lượng</th>
                             <th scope="col">Đơn vị tính</th>
                             <th scope="col">Màu sắc</th>
                             <th scope="col">Mùi thơm</th>
+                            <th scope="col">Chi tiết khác</th>
                             <th scope="col">Bảo quản</th>
                             <th scope="col">Trạng thái</th>
                             <th scope="col">Hành động</th>
@@ -173,12 +178,14 @@
                         @foreach($datas as $data)
                             <tr>
                                 <th scope="row">{{ $loop->index + 1 }}</th>
-                                <td>{{ $data->code }}- {{ $data->ten_nguyen_lieu }}</td>
-                                <td>{{ $data->PhieuSanXuat->code }} - {{ $data->PhieuSanXuat->ten_phieu }}</td>
+                                <td>{{ \Carbon\Carbon::parse($data->ngay)->format('d-m-Y') }}</td>
+                                <td>{{ $data->PhieuSanXuat->so_lo_san_xuat }}</td>
+                                <td>{{ $data->ten_nguyen_lieu }}</td>
                                 <td>{{ number_format($data->khoi_luong) }} kg</td>
                                 <td>{{ $data->don_vi_tinh }}</td>
                                 <td>{{ $data->mau_sac }}</td>
                                 <td>{{ $data->mui_thom }}</td>
+                                <td>{{ $data->chi_tiet_khac }}</td>
                                 <td>{{ $data->bao_quan }}</td>
                                 <td>{{ $data->trang_thai }}</td>
                                 <td>
