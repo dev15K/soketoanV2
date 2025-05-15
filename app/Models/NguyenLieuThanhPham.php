4d4c579cf294f766a97cacc54b851b8e676f9827
dainq <97323194+dainqd@users.noreply.github.com>
2025-05-15 23:59:31 +0700

    feat: nguyen lieu thanh pham

--- app/Models/NguyenLieuThanhPham.php
+++ app/Models/NguyenLieuThanhPham.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class NguyenLieuThanhPham extends Model
 {
     use HasFactory;
+
+    public function sanPham()
+    {
+        return $this->belongsTo(SanPham::class, 'san_pham_id', 'id');
+    }
+
+    public function nguyenLieuSanXuat()
+    {
+        return $this->belongsTo(NguyenLieuSanXuat::class, 'nguyen_lieu_san_xuat_id', 'id');
+    }
 }
--- resources/views/admin/pages/nguyen_lieu_thanh_pham/detail.blade.php
+++ resources/views/admin/pages/nguyen_lieu_thanh_pham/detail.blade.php
@@ -27,11 +27,6 @@
                           action="{{ route('admin.nguyen.lieu.thanh.pham.update', $nguyenLieuThanhPham->id) }}">
                         @method('PUT')
                         @csrf
-                        <div class="form-group">
-                            <label for="ten_san_pham">Tên sản phẩm</label>
-                            <input type="text" class="form-control" id="ten_san_pham" name="ten_san_pham"
-                                   value="{{ $nguyenLieuThanhPham->ten_san_pham }}" required>
-                        </div>
                         <div class="row">
                             <div class="form-group col-md-4">
                                 <label for="ngay">Ngày nhập kho</label>
@@ -39,79 +34,52 @@
                                        value="{{ \Carbon\Carbon::parse($nguyenLieuThanhPham->ngay)->format('Y-m-d') }}"
                                        required>
                             </div>
+
                             <div class="form-group col-md-4">
-                                <label for="ngay_san_xuat">Ngày sản xuất</label>
-                                <input type="date" class="form-control" id="ngay_san_xuat" name="ngay_san_xuat"
-                                       value="{{ \Carbon\Carbon::parse($nguyenLieuThanhPham->ngay_san_xuat)->format('Y-m-d') }}"
-                                       required>
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label for="han_su_dung">Hạn sử dụng</label>
-                                <input type="date" class="form-control" id="han_su_dung" name="han_su_dung"
-                                       value="{{ \Carbon\Carbon::parse($nguyenLieuThanhPham->han_su_dung)->format('Y-m-d') }}"
-                                       required>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="form-group col-md-6">
-                                <label for="type">Loại nguyên liệu</label>
-                                <select name="type" id="type" class="form-control">
-                                    <option
-                                        {{ $nguyenLieuThanhPham->type == 'san_xuat' ? 'selected' : '' }} value="san_xuat">
-                                        Nguyên liệu Sản xuất
-                                    </option>
-                                    <option {{ $nguyenLieuThanhPham->type == 'tho' ? 'selected' : '' }} value="tho">
-                                        Nguyên liệu Thô
-                                    </option>
-                                    <option
-                                        {{ $nguyenLieuThanhPham->type == 'phan_loai' ? 'selected' : '' }} value="phan_loai">
-                                        Nguyên liệu Phân loại
-                                    </option>
-                                    <option {{ $nguyenLieuThanhPham->type == 'tinh' ? 'selected' : '' }} value="tinh">
-                                        Nguyên liệu Tinh
-                                    </option>
+                                <label for="nguyen_lieu_san_xuat_id">Lô SX</label>
+                                <select name="nguyen_lieu_san_xuat_id" id="nguyennguyen_lieu_san_xuat_id_lieu_id"
+                                        class="form-control">
+                                    @foreach($nlsanxuats as $nlsanxuat)
+                                        <option {{ $nlsanxuat->id == $nguyenLieuThanhPham->nguyen_lieu_san_xuat_id ? 'selected' : '' }}
+                                            value="{{ $nlsanxuat->id }}">
+                                            {{ $nlsanxuat->PhieuSanXuat->so_lo_san_xuat }}
+                                            - {{ $nlsanxuat->ten_nguyen_lieu }}
+                                        </option>
+                                    @endforeach
                                 </select>
                             </div>
-                            <div class="form-group col-md-6">
-                                <label for="nguyen_lieu_id">Tên nguyên liệu</label>
-                                <select name="nguyen_lieu_id" id="nguyen_lieu_id" class="form-control">
-                                    @foreach($nlsanxuats as $nlsanxuat)
+                            <div class="form-group col-md-4">
+                                <label for="san_pham_id">Mã sản phẩm</label>
+                                <select name="san_pham_id" id="san_pham_id" class="form-control">
+                                    @foreach($products as $product)
                                         <option
-                                            {{ $nlsanxuat->id == $nguyenLieuThanhPham->nguyen_lieu_id ? 'selected' : '' }}
-                                            value="{{ $nlsanxuat->id }}">{{ $nlsanxuat->ten_nguyen_lieu }}</option>
+                                            {{ $product->id == $nguyenLieuThanhPham->san_pham_id ? 'selected' : '' }}
+                                            value="{{ $product->id }}">
+                                            {{ $product->ma_san_pham}} - {{ $product->ten_san_pham }}
+                                        </option>
                                     @endforeach
                                 </select>
                             </div>
                         </div>
                         <div class="row">
                             <div class="form-group col-md-4">
-                                <label for="khoi_luong">Khối lượng(kg)</label>
-                                <input type="number" min="0" class="form-control" id="khoi_luong" name="khoi_luong"
-                                       value="{{ $nguyenLieuThanhPham->khoi_luong }}" required>
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label for="don_vi_tinh">Đơn vị tính</label>
-                                <input type="text" class="form-control" id="don_vi_tinh" name="don_vi_tinh"
-                                       value="{{ $nguyenLieuThanhPham->don_vi_tinh }}" required>
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label for="so_luong">Số lượng</label>
+                                <label for="so_luong">Số lượng (cái/hộp)</label>
                                 <input type="text" class="form-control" id="so_luong" name="so_luong"
                                        value="{{ $nguyenLieuThanhPham->so_luong }}" required>
                             </div>
-                        </div>
-                        <div class="row">
                             <div class="form-group col-md-4">
                                 <label for="price">Giá xuất kho thương mại</label>
                                 <input type="number" class="form-control" id="price" name="price"
                                        value="{{ $nguyenLieuThanhPham->price }}" required>
                             </div>
                             <div class="form-group col-md-4">
-                                <label for="total_price">Tổng giá</label>
+                                <label for="total_price">Tổng tiền</label>
                                 <input type="number" class="form-control" id="total_price"
                                        value="{{ $nguyenLieuThanhPham->total_price }}" name="total_price" required>
                             </div>
-                            <div class="form-group col-md-4">
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
                                 <label for="trang_thai">Trạng thái</label>
                                 <select id="trang_thai" name="trang_thai" class="form-control">
                                     <option
@@ -121,6 +89,12 @@
                                         {{ $nguyenLieuThanhPham->trang_thai == \App\Enums\TrangThaiNguyenLieuThanhPham::INACTIVE() ? 'selected' : '' }}
                                         value="{{ \App\Enums\TrangThaiNguyenLieuThanhPham::INACTIVE() }}">{{ \App\Enums\TrangThaiNguyenLieuThanhPham::INACTIVE() }}</option>
                                 </select>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="ngay_san_xuat">Ngày nhập kho</label>
+                                <input type="date" class="form-control" id="ngay_san_xuat" name="ngay_san_xuat"
+                                       value="{{ \Carbon\Carbon::parse($nguyenLieuThanhPham->ngay_san_xuat)->format('Y-m-d') }}"
+                                       required>
                             </div>
                         </div>
                         <div class="form-group">
