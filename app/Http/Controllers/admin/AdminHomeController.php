74d41a1db41092cfd54def3e086bfafc3a9098a0
dainqd <dainq@absoltech.vn>
2025-07-16 16:59:05 +0700

    feat: update code

--- app/Http/Controllers/admin/AdminHomeController.php
+++ app/Http/Controllers/admin/AdminHomeController.php
@@ -51,6 +51,7 @@ class AdminHomeController extends Controller
             ->when($loai_quy_search, function ($query) use ($loai_quy_search) {
                 return $query->where('loai_quy_id', $loai_quy_search);
             })
+            ->where('so_tien', '>', 0)
             ->orderByDesc('id')
             ->paginate(10);
 
